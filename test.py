83319025db6b7889c7cc26afe3598ec80cc08f03
Muhammad Mahmoud <programmer.muhammas@gmail.com>
2023-06-18 22:53:14 +0300

    chunksize

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 print("hdddd")
+import os 
+
+SR=os.environ.get('SRC_BASE_DIR')
+name=os.environ.get('DB_HOST')
+print(name)
 n=int(input("Enter number"))
 
 if n%2 ==0 :
     print("even")
 else :
     print ("odd")
+
